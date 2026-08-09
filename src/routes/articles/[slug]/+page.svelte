3e1a86423ef7b3dd8f889e8443c4afa551de8389
S. Mehran M. Ziabary <softnhard.es@gmail.com>
2026-08-09 22:33:26 +0330

    Import Virgool archive with local images

--- src/routes/articles/[slug]/+page.svelte
+++ src/routes/articles/[slug]/+page.svelte
@@ -10,10 +10,14 @@
 <main class="article-page">
   <article>
     <header class="article-header wrap"><div class="article-meta"><span>{data.article.category}</span><span>{data.article.faDate}</span><span>{data.article.readTime}</span></div><h1>{data.article.title}</h1><p>{data.article.excerpt}</p></header>
-    <div class="article-cover"><div><span>{data.article.category}</span><b>{data.article.slug.includes('secure') ? 'ZTAI' : 'AI / SYSTEMS'}</b></div></div>
+    {#if data.article.cover}
+      <figure class="article-cover has-image"><img src={data.article.cover} alt={data.article.title} /></figure>
+    {:else}
+      <div class="article-cover"><div><span>{data.article.category}</span><b>{data.article.slug.includes('secure') ? 'ZTAI' : 'AI / SYSTEMS'}</b></div></div>
+    {/if}
     <div class="prose">
       {#if Content}<svelte:component this={Content} />{/if}
-      {#if data.article.external}<a class="original-link" href={data.article.external} target="_blank" rel="noreferrer">مطالعه نسخه منتشرشده در رسانه ↗</a>{/if}
+      {#if data.article.external}<a class="original-link" href={data.article.external} target="_blank" rel="noreferrer">مشاهده نسخه اصلی در ویرگول ↗</a>{/if}
     </div>
   </article>
   <RelatedStream related={data.article.related} />
